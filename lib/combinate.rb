42d42a593947d4020991d6576536c62e6cceae75
Ed Carrel <ed@instacart.com>
2018-04-24 22:13:13 -0700

    Fixing hash exploding bug.

--- lib/combinate.rb
+++ lib/combinate.rb
@@ -5,15 +5,28 @@ module Combinate
     perms = []
     (1.upto(arrs.length)).each do |x|
       arrs.combination(x).each do |comb|
+        first = if comb[0].kind_of?(Hash)
+                  [comb[0]]
+                else
+                  Array(comb[0])
+                end
         if comb.length == 1
-          Array(comb[0])
+          first
         else
           rest = comb[1..-1].map do |e|
-            Array(e)
+            if e.kind_of?(Hash)
+              [e]
+            else
+              Array(e)
+            end
           end
-          Array(comb[0]).product(*rest)
+          first.product(*rest)
         end.each do |p|
-          perms << Array(p)
+          perms << if p.kind_of?(Hash)
+                      [p]
+                   else
+                      Array(p)
+                    end
         end
       end
     end
--- spec/combinate_spec.rb
+++ spec/combinate_spec.rb
@@ -18,4 +18,13 @@ RSpec.describe Combinate do
   it "permutates complicated examples arrays" do
     expect(described_class.combinate([[1,2],[3,4],[5,6]])).to eq([[1], [2], [3], [4], [5], [6], [1, 3], [1, 4], [2, 3], [2, 4], [1, 5], [1, 6], [2, 5], [2, 6], [3, 5], [3, 6], [4, 5], [4, 6], [1, 3, 5], [1, 3, 6], [1, 4, 5], [1, 4, 6], [2, 3, 5], [2, 3, 6], [2, 4, 5], [2, 4, 6]])
   end
+
+  it "permutates complicated examples arrays" do
+    expect(described_class.combinate([[1,2],[3,4],[5,6]])).to eq([[1], [2], [3], [4], [5], [6], [1, 3], [1, 4], [2, 3], [2, 4], [1, 5], [1, 6], [2, 5], [2, 6], [3, 5], [3, 6], [4, 5], [4, 6], [1, 3, 5], [1, 3, 6], [1, 4, 5], [1, 4, 6], [2, 3, 5], [2, 3, 6], [2, 4, 5], [2, 4, 6]])
+  end
+  it "leaves embedded hashes alone" do
+    input = [[{a: :b}, {c: :d}],[3,4],[5,6]]
+    output =[[{:a=>:b}], [{:c=>:d}], [3], [4], [5], [6], [{:a=>:b}, 3], [{:a=>:b}, 4], [{:c=>:d}, 3], [{:c=>:d}, 4], [{:a=>:b}, 5], [{:a=>:b}, 6], [{:c=>:d}, 5], [{:c=>:d}, 6], [3, 5], [3, 6], [4, 5], [4, 6], [{:a=>:b}, 3, 5], [{:a=>:b}, 3, 6], [{:a=>:b}, 4, 5], [{:a=>:b}, 4, 6], [{:c=>:d}, 3, 5], [{:c=>:d}, 3, 6], [{:c=>:d}, 4, 5], [{:c=>:d}, 4, 6]] 
+    expect(described_class.combinate(input)).to eq(output)
+  end
 end
